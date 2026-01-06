6ebd65444a3176329a61f28ead4d27b570b559ae
ABackerNINI <abacker.nini@foxmail.com>
2026-01-05 18:27:57 -0800

    add cli arguments parsing for the install.sh

--- src/install.sh
+++ src/install.sh
@@ -46,32 +46,115 @@ function check_error() {
 
 setup
 
+set -e  # Exit on error
+
+function usage() {
+    cat << EOF
+Usage: $0 [OPTION]...
+
+OPTIONS:
+    -h, --help          Display this help and exit.
+    -d, --debug         Enable debug mode.
+    -c, --compiler      Specify the compiler to use.
+    -s, --std           Specify the C++ standard to use.
+
+EXAMPLES:
+    $0 -d --compiler=g++ --std=c++17
+EOF
+    exit 0
+}
+
+# -o: short option
+# --long: long option
+# -n: program name
+# --: end of options
+# "$0": program name
+# "$@": all arguments
+TEMP=$(getopt -o hdc:s: --long help,debug,compiler:,std: -n "$0" -- "$@")
+
+# Check for errors in argument parsing. If getopt returns a non-zero status, it means there was an error.
+if [[ $? -ne 0 ]]; then
+    echo "Error: there is an error in the argument parsing." >&2
+    usage
+fi
+
+# Reassign the result of getopt parsing back to "$@", so that these parsed options and arguments can be used in the script.
+eval set -- "$TEMP"
+
+DEBUG=false
+
+# The compiler to use to both compile the rcc and use inside rcc
+# COMPILER="g++"
+COMPILER="clang++"
+
+# The C++ standard to use when compiling the rcc and using inside rcc.
+# If not specified, the default is to use the latest standard supported by the compiler.
+CPP_STD=""
+
+# Parse the options and arguments
+while true; do
+    case "$1" in
+        -h|--help)
+            usage
+            ;;
+        -d|--debug)
+            DEBUG=true
+            shift
+            ;;
+        -c|--compiler)
+            COMPILER="$2"
+            shift 2
+            ;;
+        -s|--std)
+            CPP_STD="$2"
+            shift 2
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            echo "Internal Error!" >&2
+            exit 1
+            ;;
+    esac
+done
+
+# The remaining arguments
+# EXTRA_ARGS=("$@")
+
+################################################################################
+
 # For test_cpp_standard.sh
 chmod +x scripts/*.sh
 
 # The rcc cache directory
 rcc_cache_dir="$HOME/.cache/rcc"
 
-# The compiler to use to both compile the rcc and use inside rcc
-# compiler="g++"
-compiler="clang++"
+echo "${YELLOW}Using compiler:${NORMAL} ${UNDERLINE}$COMPILER${NORMAL}"
 
-echo "${YELLOW}Using compiler:${NORMAL} ${UNDERLINE}$compiler${NORMAL}"
-
-# Get C++ highest standard
-echo "${YELLOW}Testing C++ standard support of${NORMAL} ${UNDERLINE}$compiler${NORMAL}"
-cpp_std=$(scripts/test_cpp_standard.sh "$compiler") || exit 1
-echo "${YELLOW}Found C++ standard:${NORMAL} ${UNDERLINE}$cpp_std${NORMAL}"
+if [ -z "$CPP_STD" ]; then
+    # Get C++ highest standard
+    echo "${YELLOW}Testing C++ standard support of${NORMAL} ${UNDERLINE}$COMPILER${NORMAL}"
+    CPP_STD=$(scripts/test_cpp_standard.sh "$COMPILER") || exit 1
+    echo "${YELLOW}Found C++ standard:${NORMAL} ${UNDERLINE}$CPP_STD${NORMAL}"
+fi
 
 echo "${YELLOW}Cache directory:${NORMAL} ${UNDERLINE}$rcc_cache_dir${NORMAL}"
+echo "${YELLOW}DEBUG:${NORMAL} ${UNDERLINE}$DEBUG${NORMAL}"
 echo ""
 
 # Build rcc
-echo "${YELLOW}Building rcc with${NORMAL} ${UNDERLINE}$compiler${NORMAL} and ${UNDERLINE}$cpp_std${NORMAL}"
+echo "${YELLOW}Building rcc with${NORMAL} ${UNDERLINE}$COMPILER${NORMAL} and ${UNDERLINE}$CPP_STD${NORMAL}"
 make clean
 check_error "make clean"
-make release "CPP_COMPILER=$compiler" "CPP_STD=$cpp_std" "RCC_CACHE_DIR=$rcc_cache_dir"
-check_error "make release"
+if [ $DEBUG == true ]; then
+    make debug "CPP_COMPILER=$COMPILER" "CPP_STD=$CPP_STD" "RCC_CACHE_DIR=$rcc_cache_dir"
+    check_error "make debug"
+else
+    make release "CPP_COMPILER=$COMPILER" "CPP_STD=$CPP_STD" "RCC_CACHE_DIR=$rcc_cache_dir"
+    check_error "make release"
+fi
 
 # Copy rcc to path
 echo "${YELLOW}Installing rcc to /usr/local/bin/${NORMAL}"
@@ -93,7 +176,7 @@ mkdir -p "$rcc_cache_dir/templates"
 check_error "mkdir -p \"$rcc_cache_dir/templates\""
 
 # Copy templates to rcc cache dir
-make -C template clean "CPP_COMPILER=$compiler"
+make -C template clean "CPP_COMPILER=$COMPILER"
 check_error "make clean"
 echo "${YELLOW}Copying templates to cache directory${NORMAL}"
 cp -r template/* -t "$rcc_cache_dir/templates"
@@ -101,7 +184,7 @@ check_error "cp -r template/* -t \"$rcc_cache_dir/templates\""
 
 # Build Pre-Compiled Header
 echo "${YELLOW}Building Pre-Compiled Header${NORMAL}"
-make -C "$rcc_cache_dir/templates" "CPP_COMPILER=$compiler" "CPP_STD=$cpp_std"
+make -C "$rcc_cache_dir/templates" "CPP_COMPILER=$COMPILER" "CPP_STD=$CPP_STD"
 check_error "make"
 
 echo ""
