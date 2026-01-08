2be1216f99b3a9ab79b719028b24a44067e5fa62
ABackerNINI <abacker.nini@foxmail.com>
2026-01-08 00:34:26 -0800

    improve IDE auto-complete experence of using debug_fmt

--- src/debug_fmt.h
+++ src/debug_fmt.h
@@ -5,44 +5,29 @@
 
 #ifdef DEBUG
     #include "libs/fmt/color.h"
-    #include "libs/fmt/core.h"
+    #include <cassert>
 
-    #define gprint(...)                                                                                                \
-        do {                                                                                                           \
-            fmt::print(stderr, fmt::fg(fmt::color::green), "[DEBUG] ");                                                \
-            fmt::print(stderr, __VA_ARGS__);                                                                           \
-        } while (0)
-
-    #define gprintc(...)                                                                                               \
-        do {                                                                                                           \
-            fmt::print(stderr, fmt::fg(fmt::color::green), " >      ");                                                \
-            fmt::print(stderr, __VA_ARGS__);                                                                           \
-        } while (0)
-
-    #define gstmt(...)                                                                                                 \
+    #define __DBG_PRT_REAPEAT(...)                                                                                     \
         do {                                                                                                           \
             __VA_ARGS__;                                                                                               \
         } while (0)
-
-    #include <cassert>
-
-    #define grequire(...) assert(__VA_ARGS__)
-    #define gassert(...) assert(__VA_ARGS__)
-    #define gensure(...) assert(__VA_ARGS__)
-
 #else
-    /* Not enabled. */
-    #define gprint(...)
-    /* Not enabled. */
-    #define gprintc(...)
-    /* Not enabled. */
-    #define gstmt(...)
-    /* Not enabled. */
-    #define grequire(...)
-    /* Not enabled. */
-    #define gassert(...)
-    /* Not enabled. */
-    #define gensure(...)
+    #define __DBG_PRT_REAPEAT(...)                                                                                     \
+        while (0) {                                                                                                    \
+            __VA_ARGS__;                                                                                               \
+        }
 #endif
+
+#define gprint(...)                                                                                                    \
+    __DBG_PRT_REAPEAT(fmt::print(stderr, fmt::fg(fmt::color::green), "[DEBUG] "); fmt::print(stderr, __VA_ARGS__);)
+
+#define gprintc(...)                                                                                                   \
+    __DBG_PRT_REAPEAT(fmt::print(stderr, fmt::fg(fmt::color::green), " >      "); fmt::print(stderr, __VA_ARGS__);)
+
+#define gstmt(...) __DBG_PRT_REAPEAT(__VA_ARGS__;)
+
+#define grequire(...) __DBG_PRT_REAPEAT(assert(__VA_ARGS__))
+#define gassert(...) __DBG_PRT_REAPEAT(assert(__VA_ARGS__))
+#define gensure(...) __DBG_PRT_REAPEAT(assert(__VA_ARGS__))
 
 #endif // __DEBUG_PRINT_FMT_H__
