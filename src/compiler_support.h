15aca1bbe53a367a61cb0f268b5660551293f794
ABackerNINI <abacker.nini@foxmail.com>
2026-01-19 15:28:02 -0800

    update comment

--- src/compiler_support.h
+++ src/compiler_support.h
@@ -79,7 +79,6 @@ class linux_clang : public compiler_support {
 };
 
 // Create a new compiler support object based on the compiler name.
-// The returned object should be deleted by the caller.
 std::unique_ptr<compiler_support> create_compiler_support(const std::string &compiler_name, const Settings &settings);
 
 } // namespace rcc
