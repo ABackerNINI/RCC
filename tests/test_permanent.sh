0f339bedbca0d36865501de381d8db8dc3f5f4a5
ABackerNINI <abacker.nini@foxmail.com>
2026-01-23 12:49:39 -0800

    add tests for permanent options

--- tests/test_permanent.sh
+++ tests/test_permanent.sh
@@ -2,6 +2,28 @@
 
 source utils.sh
 
+# Test permanent options
+
+rcc --permanent test_permanent --desc "This is a test permanent" 'cout<<"This is a test message for test_permanent"<<endl;'
+check_error "rcc --permanent"
+
+rcc --run-permanent test_permanent >out.txt 2>err.txt
+check_error "rcc --run-permanent"
+
+diff <(echo "This is a test message for test_permanent") out.txt
+check_error "checking rcc --run-permanent output"
+
+rcc --list-permanent | grep test_permanent
+check_error "rcc --list-permanent"
+
+rcc --remove-permanent test_permanent
+check_error "rcc --remove-permanent"
+
+# Test permanent subcommands
+
+rcc list | grep test_permanent
+check_error "rcc list after remove" "1" true
+
 rcc create test_permanent --desc "This is a test permanent" 'cout<<"This is a test message for test_permanent"<<endl;'
 check_error "rcc create"
 
